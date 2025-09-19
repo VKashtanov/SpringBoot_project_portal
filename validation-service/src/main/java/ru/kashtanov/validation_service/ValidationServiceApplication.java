accdcca4838c78f1ab8b254507e967dcd3a46fb3
Viktor Kashtanov <pistoletviktor@gmail.com>
2025-09-19 10:11:47 +0300

    initial commit

---
+++ validation-service/src/main/java/ru/kashtanov/validation_service/ValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.kashtanov.validation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationServiceApplication.class, args);
+	}
+
+}
---
+++ validation-service/src/test/java/ru/kashtanov/validation_service/ValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kashtanov.validation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
